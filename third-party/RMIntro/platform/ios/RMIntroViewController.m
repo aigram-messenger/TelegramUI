be79e91792b9f7905fda390a61e3de86151b22dd
Vladislav Sedinkin <vsedinkin@smedialink.com>
2019-03-05 15:35:33 +0600

    Revert "- updated onboarding powerful;"

--- third-party/RMIntro/platform/ios/RMIntroViewController.m
+++ third-party/RMIntro/platform/ios/RMIntroViewController.m
@@ -148,18 +148,29 @@ static void TGDispatchOnMainThread(dispatch_block_t block) {
         }
         _englishStrings = englishStrings;
         
-        _headlines = @[ _englishStrings[@"Tour.Title1"], _englishStrings[@"Tour.Title2"], _englishStrings[@"Tour.Title3"], _englishStrings[@"Tour.Title4"],
-                        _englishStrings[@"Tour.Title5"], /*_englishStrings[@"Tour.Title6"]*/];
-        _descriptions = @[_englishStrings[@"Tour.Text1"], _englishStrings[@"Tour.Text2"], _englishStrings[@"Tour.Text3"], _englishStrings[@"Tour.Text4"],
-                          _englishStrings[@"Tour.Text5"], /*_englishStrings[@"Tour.Text6"]*/];
-        
+        _headlines = @[_englishStrings[@"Tour.Title1"],
+                       _englishStrings[@"Tour.Title2"],
+                       _englishStrings[@"Tour.Title3"],
+                       _englishStrings[@"Tour.Title4"],
+                       _englishStrings[@"Tour.Title5"],
+                       _englishStrings[@"Tour.Title6"]
+                       ];
+        _descriptions = @[
+                          _englishStrings[@"Tour.Text1"],
+                          _englishStrings[@"Tour.Text2"],
+                          _englishStrings[@"Tour.Text3"],
+                          _englishStrings[@"Tour.Text4"],
+                          _englishStrings[@"Tour.Text5"],
+                          _englishStrings[@"Tour.Text6"]
+                          ];
         _imageNames = @[
-                    @"onboard_first",
-                    @"onboard_fast",
-                    @"onboard_smart",
-                    @"onboard_secure",
-                    @"onboard_cloud"
-                    ];
+                        @"onboard_first",
+                        @"onboard_fast",
+                        @"onboard_smart",
+                        @"onboard_powerful",
+                        @"onboard_secure",
+                        @"onboard_cloud"
+                        ];
         
         __weak RMIntroViewController *weakSelf = self;
         _didEnterBackgroundObserver = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidEnterBackgroundNotification object:nil queue:nil usingBlock:^(__unused NSNotification *notification)
